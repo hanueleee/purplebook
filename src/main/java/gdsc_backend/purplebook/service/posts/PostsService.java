66bf4e12b371bb9b539efd5d756c645fb790b221
hanuel <ezzeezz@naver.com>
2022-11-24 21:53:21 +0900

    feat(posts): 수정 및 조회를 위해 entity와 service에 추가 기능 구현

--- src/main/java/gdsc_backend/purplebook/service/posts/PostsService.java
+++ src/main/java/gdsc_backend/purplebook/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package gdsc_backend.purplebook.service.posts;
 
+import gdsc_backend.purplebook.domain.posts.Posts;
 import gdsc_backend.purplebook.domain.posts.PostsRepository;
+import gdsc_backend.purplebook.web.dto.PostsResponseDto;
 import gdsc_backend.purplebook.web.dto.PostsSaveRequestDto;
+import gdsc_backend.purplebook.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
