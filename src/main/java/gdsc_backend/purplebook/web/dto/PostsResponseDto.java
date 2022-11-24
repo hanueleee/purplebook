b6b5322072deb22ad7669a20cb6df032756a6edb
hanuel <ezzeezz@naver.com>
2022-11-24 21:54:25 +0900

    feat(dto): update를 위한 dto와 response용 dto 구현

--- src/main/java/gdsc_backend/purplebook/web/dto/PostsResponseDto.java
+++ src/main/java/gdsc_backend/purplebook/web/dto/PostsResponseDto.java
@@ -1,2 +1,19 @@
-package gdsc_backend.purplebook.web.dto;public class PostsResponseDto {
+package gdsc_backend.purplebook.web.dto;
+
+import gdsc_backend.purplebook.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
 }
--- src/main/java/gdsc_backend/purplebook/web/dto/PostsUpdateRequestDto.java
+++ src/main/java/gdsc_backend/purplebook/web/dto/PostsUpdateRequestDto.java
@@ -1,2 +1,18 @@
-package gdsc_backend.purplebook.web.dto;public class PostsUpdateRequestDto {
+package gdsc_backend.purplebook.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
