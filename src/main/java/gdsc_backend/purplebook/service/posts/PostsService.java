acbd62409d70c9907d4cc728633fe9d7b525406a
hanuel <ezzeezz@naver.com>
2022-11-24 21:17:25 +0900

    feat(posts): Add PostsService

--- src/main/java/gdsc_backend/purplebook/service/posts/PostsService.java
+++ src/main/java/gdsc_backend/purplebook/service/posts/PostsService.java
@@ -1,2 +1,18 @@
-package gdsc_backend.purplebook.service.posts;public class PostsService {
+package gdsc_backend.purplebook.service.posts;
+
+import gdsc_backend.purplebook.domain.posts.PostsRepository;
+import gdsc_backend.purplebook.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
