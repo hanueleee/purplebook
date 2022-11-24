d31f1968b002ad0277888135753b2d93ab06b3fc
hanuel <ezzeezz@naver.com>
2022-11-25 01:53:05 +0900

    feat: JPA Auditing 기능 적용

--- src/main/java/gdsc_backend/purplebook/PurplebookApplication.java
+++ src/main/java/gdsc_backend/purplebook/PurplebookApplication.java
@@ -2,12 +2,13 @@ package gdsc_backend.purplebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PurplebookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PurplebookApplication.class, args);
 	}
-
 }
--- src/main/java/gdsc_backend/purplebook/domain/posts/Posts.java
+++ src/main/java/gdsc_backend/purplebook/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package gdsc_backend.purplebook.domain.posts;
 
+import gdsc_backend.purplebook.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
