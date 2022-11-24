6ee80a3e86f3ec24d08f3920e702820b0f4cdd3e
hanuel <ezzeezz@naver.com>
2022-11-24 20:04:28 +0900

    feat(web): 테스트용 컨트롤러

---
+++ src/main/java/gdsc_backend/purplebook/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package gdsc_backend.purplebook.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
