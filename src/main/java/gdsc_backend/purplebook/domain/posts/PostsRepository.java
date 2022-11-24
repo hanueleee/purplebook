1ce9112bb8fa2a66d032affd8c528ca32815c169
hanuel <ezzeezz@naver.com>
2022-11-24 20:05:56 +0900

    feat(Posts): Posts 도메인과 repository

---
+++ src/main/java/gdsc_backend/purplebook/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package gdsc_backend.purplebook.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
